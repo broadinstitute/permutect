a3f1cbe76d12173773b74380bd4cba6dfd44a8a2
David Benjamin <davidben@broadinstitute.org>
2026-01-08 09:38:38 -0500

    Improved normalization of variant (info) features (#254)
    * all GATK INFO features except TLOD / alt count are made binary / categorical, not quantile normalized
    * STR INFO is binary / categorical
    * use a corrected TLOD / alt count and don't quantile normalize it.  Only remaining quantile norm is fragment length

--- permutect/data/plain_text_data.py
+++ permutect/data/plain_text_data.py
@@ -35,6 +35,7 @@ from queue import PriorityQueue
 from typing import List, Generator
 
 import numpy as np
+from scipy.special import gammaln
 from sklearn.preprocessing import QuantileTransformer
 
 from permutect.data.count_binning import cap_ref_count, cap_alt_count
@@ -49,6 +50,7 @@ from permutect.utils.enums import Variation, Label
 MAX_VALUE = 10000
 EPSILON = 0.00001
 QUANTILE_DATA_COUNT = 10000
+LOG10_TO_LN = 2.30258509299
 
 RAW_READS_DTYPE = DEFAULT_NUMPY_FLOAT
 MIN_NUM_DATA_FOR_NORMALIZATION = 1000
@@ -56,6 +58,16 @@ MAX_NUM_DATA_FOR_NORMALIZATION = 100000
 NUM_RAW_DATA_TO_NORMALIZE_AT_ONCE = 10000
 
 DISTANCE_FROM_END_SATURATION = 20
+
+def binarize_str_length_column(str_length_column):
+    result = np.full((len(str_length_column), 6), 0, dtype=str_length_column.dtype)
+    result[:, 0] = (str_length_column == 1)
+    result[:, 1] = (str_length_column > 1) & (str_length_column < 4)
+    result[:, 2] = (str_length_column > 3) & (str_length_column < 6)
+    result[:, 3] = (str_length_column > 5) & (str_length_column < 9)
+    result[:, 4] = (str_length_column > 8) & (str_length_column < 13)
+    result[:, 5] = (str_length_column > 12)
+    return result
 
 
 def count_number_of_data_and_reads_in_text_file(dataset_file):
@@ -176,7 +188,7 @@ def normalized_data_generator(raw_mmap_data: MemoryMappedData) -> Generator[Read
 
         read_start_idx = 0 if start_idx == 0 else read_end_indices[start_idx - 1]
 
-        read_quantile_transform, info_quantile_transform = make_read_and_info_quantile_transforms(
+        read_quantile_transform = make_read_quantile_transform(
             read_end_indices=(read_end_indices[start_idx:end_idx]-read_start_idx), data_ve=data_mmap_ve[start_idx:end_idx],
             reads_re=reads_mmap_re[read_start_idx:read_end_indices[end_idx - 1]],)
 
@@ -186,11 +198,11 @@ def normalized_data_generator(raw_mmap_data: MemoryMappedData) -> Generator[Read
             raw_datum = RawUnnormalizedReadsDatum(datum_array=data_mmap_ve[idx], reads_re=reads)
             raw_data_list.append(raw_datum)
 
-        normalized_data_list = normalize_raw_data_list(raw_data_list, read_quantile_transform, info_quantile_transform)
+        normalized_data_list = normalize_raw_data_list(raw_data_list, read_quantile_transform)
         for datum in normalized_data_list:
             yield datum
 
-def make_read_and_info_quantile_transforms(read_end_indices, data_ve, reads_re):
+def make_read_quantile_transform(read_end_indices, data_ve, reads_re):
     read_end_indices = read_end_indices
 
     # indices_for_normalization = get_normalization_set(data_ve)
@@ -201,11 +213,6 @@ def make_read_and_info_quantile_transforms(read_end_indices, data_ve, reads_re):
     normalization_read_start_indices = [read_end_indices[max(idx - 1, 0)] for idx in indices_for_normalization]
     normalization_ref_counts = [Datum(array=data_ve[idx]).get_ref_count() for idx in indices_for_normalization]
     normalization_ref_end_indices = [(start + ref_count) for start, ref_count in zip(normalization_read_start_indices, normalization_ref_counts)]
-
-    # extract the INFO array from all the data arrays in the normalization set
-    info_for_normalization_ve = np.vstack([Datum(array=data_ve[idx]).get_info_1d() for idx in indices_for_normalization])
-    info_quantile_transform = QuantileTransformer(n_quantiles=100, output_distribution='normal')
-    info_quantile_transform.fit(info_for_normalization_ve)
 
     # for every index in the normalization set, get all the reads of the corresponding datum.  Stack all these reads to
     # obtain the reads normalization array
@@ -224,15 +231,7 @@ def make_read_and_info_quantile_transforms(read_end_indices, data_ve, reads_re):
     print(f"90: {read_quantile_transform.quantiles_[90]}")
     print(f"100: {read_quantile_transform.quantiles_[99]}")
 
-    print("Here are the percentiles of the info quantile transform:")
-    print(f"0: {info_quantile_transform.quantiles_[0]}")
-    print(f"10: {info_quantile_transform.quantiles_[10]}")
-    print(f"25: {info_quantile_transform.quantiles_[25]}")
-    print(f"50: {info_quantile_transform.quantiles_[50]}")
-    print(f"75: {info_quantile_transform.quantiles_[75]}")
-    print(f"90: {info_quantile_transform.quantiles_[90]}")
-    print(f"100: {info_quantile_transform.quantiles_[99]}")
-    return read_quantile_transform, info_quantile_transform
+    return read_quantile_transform
 
 
 def make_normalized_mmap_data(dataset_files, sources: List[int]=None) -> MemoryMappedData:
@@ -290,16 +289,74 @@ def get_normalization_set(raw_stacked_data_ve) -> List[int]:
 
 
 # this normalizes the buffer and also prepends new features to the info tensor
-def normalize_raw_data_list(buffer: List[RawUnnormalizedReadsDatum], read_quantile_transform,
-                            info_quantile_transform) -> List[ReadsDatum]:
+def normalize_raw_data_list(buffer: List[RawUnnormalizedReadsDatum], read_quantile_transform) -> List[ReadsDatum]:
     # 2D array.  Rows are ref/alt reads, columns are read features
     all_reads_re = np.vstack([datum.reads_re for datum in buffer])
 
     # 2D array.  Rows are read sets, columns are info features
     all_info_ve = np.vstack([datum.get_info_1d() for datum in buffer])
-    binary_info_columns = binary_column_indices(all_info_ve)
 
+    # INFO features from GATK are
+    # 0, 1 haplotype equivalence share of 2nd and 3rd most supported haplotype groups (among all haplotypes with
+    # the alt allele)
+    # 2 edit distance, excluding variant site, of best alt haplotype vs closest germline haplotype
+    # 3 fraction of reads supporting alt allele that support the single best alt haplotype
+    # 4 TLOD / alt count
 
+    hap_equiv_columns = all_info_ve[:, 0:2]
+    hap_equiv_binary_1 = (hap_equiv_columns < 0.1)
+    hap_equiv_binary_2 = (hap_equiv_columns >= 0.1) & (hap_equiv_columns < 0.25)
+    hap_equiv_binary_3 = (hap_equiv_columns >= 0.25)
+
+    edit_dist_column = all_info_ve[:, 2]
+    edit_dist_binary = np.full((len(all_info_ve), 4), 0, dtype=all_info_ve.dtype)
+    edit_dist_binary[:, 0] = (edit_dist_column == 0)
+    edit_dist_binary[:, 1] = (edit_dist_column == 1)
+    edit_dist_binary[:, 2] = (edit_dist_column == 2)
+    edit_dist_binary[:, 3] = (edit_dist_column >= 3)
+
+    hap_dom_column = all_info_ve[:, 3]
+    hap_dom_binary = np.full((len(all_info_ve), 2), 0, dtype=all_info_ve.dtype)
+    hap_dom_binary[:, 0] = (hap_dom_column > 0.9)
+    hap_dom_binary[:, 1] = (hap_dom_column <= 0.9) & (hap_dom_column > 0.7)
+
+    # STR INFO features are
+    # 5, 6 insertion_length, deletion_length of variant
+    # 7 STR repeat unit length
+    # 8 number of STR repeat units
+    # 9 repeats before variant
+    # 10 repeats after variant
+
+    indel_lengths_ve = all_info_ve[:, 5:7] / 10
+
+    str_unit_length_col = all_info_ve[:, 7].reshape(-1,1)
+    str_unit_length_binaries = np.hstack([str_unit_length_col == 1, str_unit_length_col == 2, str_unit_length_col == 3,
+                                str_unit_length_col == 4, str_unit_length_col >= 5])
+
+    str_total_length = all_info_ve[:, 7] * all_info_ve[:, 8]
+    str_total_length_binaries = binarize_str_length_column(str_total_length)
+
+    str_length_before = all_info_ve[:, 7] * all_info_ve[:, 9]
+    str_length_before_binaries = binarize_str_length_column(str_length_before)
+
+    str_length_after = all_info_ve[:, 7] * all_info_ve[:, 10]
+    str_length_after_binaries = binarize_str_length_column(str_length_after)
+
+    str_info_array_ve = np.hstack([indel_lengths_ve, str_unit_length_binaries, str_total_length_binaries, str_length_before_binaries, str_length_after_binaries])
+
+    binary_info_array_ve = np.hstack(
+        (hap_equiv_binary_1, hap_equiv_binary_2, hap_equiv_binary_3, edit_dist_binary, hap_dom_binary, str_info_array_ve))
+
+    tlod_over_nalt = all_info_ve[:, 4]
+    orig_alt_counts = np.array([datum.get_original_alt_count() for datum in buffer])
+    orig_depths = np.array([datum.get_original_depth() for datum in buffer])
+    orig_ref_counts = orig_depths - orig_alt_counts
+
+    natural_log_tlod = LOG10_TO_LN * tlod_over_nalt * orig_alt_counts
+    tlod_correction = gammaln(orig_depths + 2) - gammaln(orig_alt_counts + 1) - gammaln(orig_ref_counts + 1)
+    average_qual_feature = ((natural_log_tlod + tlod_correction) / orig_alt_counts)/10
+
+    all_info_transformed_ve = np.hstack([binary_info_array_ve, average_qual_feature.reshape(-1,1)])
 
     from_read_ends_columns_re = all_reads_re[:, 4:6]
     from_read_ends_transformed_re = np.tanh(from_read_ends_columns_re / DISTANCE_FROM_END_SATURATION)
@@ -309,7 +366,6 @@ def normalize_raw_data_list(buffer: List[RawUnnormalizedReadsDatum], read_quanti
     distance_columns_transformed_re = read_quantile_transform.transform(distance_columns_re)
     float_read_columns_re = np.hstack([from_read_ends_transformed_re, from_frag_ends_transformed_re, distance_columns_transformed_re])
 
-    all_info_transformed_ve = transform_except_for_binary_columns(all_info_ve, info_quantile_transform, binary_info_columns)
 
     # columns of raw read data are
     # 0 map qual -> 4 categorical columns
@@ -404,35 +460,3 @@ def read_integers(line: str):
 
 def read_float(line: str):
     return float(line.strip().split()[0])
-
-
-def is_binary(column_tensor_1d: np.ndarray):
-    assert len(column_tensor_1d.shape) == 1
-    return all(el.item() == 0 or el.item() == 1 for el in column_tensor_1d)
-
-
-def binary_column_indices(tensor_2d: np.ndarray):
-    assert len(tensor_2d.shape) == 2
-    return [n for n in range(tensor_2d.shape[1]) if is_binary(tensor_2d[:, n])]
-
-
-def non_binary_column_indices(tensor_2d: np.ndarray):
-    assert len(tensor_2d.shape) == 2
-    return [n for n in range(tensor_2d.shape[1]) if not is_binary(tensor_2d[:, n])]
-
-
-# copy the unnormalized values of binary features (columns)
-# we modify the normalized values in-place
-def restore_binary_columns(normalized, original, binary_columns):
-    result = normalized
-    if len(normalized.shape) == 2:
-        result[:, binary_columns] = original[:, binary_columns]
-    elif len(normalized.shape) == 1:
-        result[binary_columns] = original[binary_columns]
-    else:
-        raise Exception("This is only for 1D or 2D tensors")
-    return result
-
-
-def transform_except_for_binary_columns(tensor_2d, quantile_transform: QuantileTransformer, binary_column_indices):
-    return restore_binary_columns(quantile_transform.transform(tensor_2d), tensor_2d, binary_column_indices)
