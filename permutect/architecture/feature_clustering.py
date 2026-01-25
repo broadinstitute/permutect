2cfa770a501745be896e3448e12f199c929030b5
David Benjamin <davidben@broadinstitute.org>
2026-01-25 10:46:51 -0500

    two non-artifact clusters instead of one

--- permutect/architecture/feature_clustering.py
+++ permutect/architecture/feature_clustering.py
@@ -19,9 +19,10 @@ class FeatureClustering(nn.Module):
 
         self.feature_dim = feature_dimension
         self.num_artifact_clusters = num_artifact_clusters
+        self.num_nonartifact_clusters = 2
 
         # the 0th cluster is non-artifact
-        self.num_clusters = self.num_artifact_clusters + 1
+        self.num_clusters = self.num_artifact_clusters + self.num_nonartifact_clusters
 
         # num_clusters different centroids for each variant type, each a vector in feature space.  Initialize even weights.
         self.alt_centroids_vke = Parameter(torch.rand(len(Variation), self.num_clusters, self.feature_dim))
@@ -31,7 +32,9 @@ class FeatureClustering(nn.Module):
         self.alt_log_stdev_vk = Parameter(torch.zeros(len(Variation), self.num_clusters))
         self.ref_log_stdev_vk = Parameter(torch.zeros(len(Variation), self.num_clusters))
 
-        self.cluster_weights_pre_softmax_vk = Parameter(torch.ones(len(Variation), self.num_artifact_clusters))
+        # this is a single parameter for holding all pre-softmax weights, but the nonartifact weights and
+        # artifact weights are sub-tensors that are softmaxed separately
+        self.cluster_weights_pre_softmax_vk = Parameter(torch.ones(len(Variation), self.num_clusters))
 
     def log_likelihoods(self, reads_bre: RaggedSets, centroids_vke, log_stdev_vk, counts_b: IntTensor, var_types_b: IntTensor):
         reads_rke = reads_bre.flattened_tensor_nf[:, None, :]
@@ -59,9 +62,17 @@ class FeatureClustering(nn.Module):
         log_lks_bk = alt_log_lks_bk + ref_log_lks_bk
 
         # these are the log of weights that sum to 1
-        log_artifact_cluster_weights_vk = torch.log_softmax(self.cluster_weights_pre_softmax_vk, dim=-1)
+        nonartifact_weights_presoftmax_vk = self.cluster_weights_pre_softmax_vk[:, :self.num_nonartifact_clusters]
+        artifact_weights_presoftmax_vk = self.cluster_weights_pre_softmax_vk[:, self.num_nonartifact_clusters:]
+
+        log_nonartifact_cluster_weights_vk = torch.log_softmax(nonartifact_weights_presoftmax_vk, dim=-1)
+        log_artifact_cluster_weights_vk = torch.log_softmax(artifact_weights_presoftmax_vk, dim=-1)
+
+        log_nonartifact_cluster_weights_bk = log_nonartifact_cluster_weights_vk[var_types_b]
         log_artifact_cluster_weights_bk = log_artifact_cluster_weights_vk[var_types_b]
-        log_lks_bk[:, 1:] += log_artifact_cluster_weights_bk
+
+        log_lks_bk[:, :self.num_nonartifact_clusters] += log_nonartifact_cluster_weights_bk
+        log_lks_bk[:, self.num_nonartifact_clusters:] += log_artifact_cluster_weights_bk
         return log_lks_bk
 
     def calculate_logits(self, ref_bre: RaggedSets, alt_bre: RaggedSets, ref_counts_b: IntTensor, alt_counts_b: IntTensor, var_types_b: IntTensor):
