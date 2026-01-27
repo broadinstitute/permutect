4d74faabbc36851f0b24655b5e6125c34b73dc60
David Benjamin <davidben@broadinstitute.org>
2026-01-27 00:45:21 -0500

    cluster log likelihoods are modeled as linear

--- permutect/architecture/feature_clustering.py
+++ permutect/architecture/feature_clustering.py
@@ -23,38 +23,23 @@ class FeatureClustering(nn.Module):
         # the 0th cluster is non-artifact
         self.num_clusters = self.num_artifact_clusters + 1
 
-        # num_clusters different centroids for each variant type, each a vector in feature space.  Initialize even weights.
-        self.alt_centroids_vke = Parameter(torch.rand(len(Variation), self.num_clusters, self.feature_dim))
-        self.ref_centroids_vke = Parameter(torch.rand(len(Variation), self.num_clusters, self.feature_dim))
-
-        # cluster standard deviations.  Isotropic diagonal
-        self.alt_log_stdev_vk = Parameter(torch.zeros(len(Variation), self.num_clusters))
-        self.ref_log_stdev_vk = Parameter(torch.zeros(len(Variation), self.num_clusters))
+        self.alt_linear = torch.nn.Linear(in_features=self.feature_dim, out_features=self.num_clusters)
+        self.ref_linear = torch.nn.Linear(in_features=self.feature_dim, out_features=self.num_clusters)
 
         self.cluster_weights_pre_softmax_vk = Parameter(torch.ones(len(Variation), self.num_artifact_clusters))
 
-    def log_likelihoods(self, reads_bre: RaggedSets, centroids_vke, log_stdev_vk, counts_b: IntTensor, var_types_b: IntTensor, detach_reads: bool = False):
-        reads_rke = reads_bre.flattened_tensor_nf[:, None, :]
-        reads_rke = reads_rke.detach() if detach_reads else reads_rke
-        centroids_bke = centroids_vke[var_types_b]
-        centroids_rke = torch.repeat_interleave(centroids_bke, repeats=counts_b, dim=0)
-        diff_rke = reads_rke - centroids_rke
-        log_stdev_bk = log_stdev_vk[var_types_b]
-        log_stdev_rk = torch.repeat_interleave(log_stdev_bk, repeats=counts_b, dim=0)
-        stdev_rk = torch.exp(log_stdev_rk)
-
-        log_lks_rk = -self.feature_dim * log_stdev_rk  - torch.sum(torch.square(diff_rke), dim=-1) / (2 * torch.square(stdev_rk))
+    def log_likelihoods(self, reads_bre: RaggedSets, linear_trans, counts_b: IntTensor, var_types_b: IntTensor, detach_reads: bool = False):
+        reads_re = reads_bre.flattened_tensor_nf
+        log_lks_rk = linear_trans.forward(reads_re)
         log_lks_brk = RaggedSets.from_flattened_tensor_and_sizes(log_lks_rk, counts_b)
         log_lks_bk = log_lks_brk.sums_over_sets()
         return log_lks_bk
 
     def weighted_log_likelihoods_bk(self, ref_bre: RaggedSets, alt_bre: RaggedSets, ref_counts_b: IntTensor, alt_counts_b: IntTensor, var_types_b: IntTensor, detach_reads: bool = False):
 
-        alt_log_lks_bk = self.log_likelihoods(reads_bre=alt_bre, centroids_vke=self.alt_centroids_vke,
-                                              log_stdev_vk=self.alt_log_stdev_vk, counts_b=alt_counts_b, var_types_b=var_types_b, detach_reads=detach_reads)
+        alt_log_lks_bk = self.log_likelihoods(reads_bre=alt_bre, linear_trans=self.alt_linear, counts_b=alt_counts_b, var_types_b=var_types_b, detach_reads=detach_reads)
 
-        ref_log_lks_bk = self.log_likelihoods(reads_bre=ref_bre, centroids_vke=self.ref_centroids_vke,
-                                              log_stdev_vk=self.ref_log_stdev_vk, counts_b=ref_counts_b,
+        ref_log_lks_bk = self.log_likelihoods(reads_bre=ref_bre, linear_trans=self.ref_linear, counts_b=ref_counts_b,
                                               var_types_b=var_types_b, detach_reads=detach_reads)
 
         log_lks_bk = alt_log_lks_bk + ref_log_lks_bk
